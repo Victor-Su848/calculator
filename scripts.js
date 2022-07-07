44a8c6fbf582c0ac1db643b8f2f17e5d792898e6
Victor Su <victor.yuchen.su@gmail.com>
2022-07-07 18:00:50 -0400

    created operator functions and basic html

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+function divide(num1, num2) {
+    return num1 / num2;
+}
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+}
+
+const oneBut = document.querySelector('#one');
+const twoBut = document.querySelector('#two');
+const threeBut = document.querySelector('#three');
+const fourBut = document.querySelector('#four');
+const fiveBut = document.querySelector('#five');
+const sixBut = document.querySelector('#six');
+const sevenBut = document.querySelector('#seven');
+const eightBut = document.querySelector('#eight');
+const nineBut = document.querySelector('#nine');
+
+
+//updates display after enter button is clicked
+function display() {
+
+}
